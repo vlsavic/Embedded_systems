434741aa7963ca3396156d157f20a3a588c8f6b7
Vlado <ynwa.liv@gmail.com>
2019-11-21 14:37:23 +0100

    First lesson was finished!

---
+++ lesson1/README.txt
@@ -0,0 +1,6 @@
+#This is a first lesson about embedded programing.
+#
+#We will use ARM CortexM microcontroller for studing.
+#ARM CortexM is 32bit processor with 16 registers (R0-R14 and PC)
+#PC -> Program Counter
+
---
+++ lesson1/main.c
@@ -0,0 +1,19 @@
+
+int main(){
+  int counter = 0;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  ++counter;
+  return 0;
+}
